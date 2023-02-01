f0538e21a6749dbcce0ac9677ffcafae945dc506
Tsikhon Bazar <103754328+tihon2005@users.noreply.github.com>
2023-02-02 01:50:29 +0300

    Add files via upload

---
+++ MyApp/MyApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Tikhon Bazar on 1.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var numberLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        numberLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        numberLabel.text = "0"
+    }
+
+    @IBAction func plusTapped(_ sender: UIButton) {
+        numberLabel.text = String((Int(numberLabel.text ?? "0") ?? 0) + 1)
+    }
+    
+    @IBAction func minusTapped(_ sender: UIButton) {
+        numberLabel.text = String((Int(numberLabel.text ?? "0") ?? 0) - 1)
+    }
+}
+
